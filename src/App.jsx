4413406b1e6691ccee8d39c8e1da40098edd6d9f
johaoDev <johaobravoespinoza1501@gmail.com>
2025-07-22 13:34:09 -0500

    defining the color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        zuzz Dev
-      </h1>
-    </>
-  )
+    <div className="flex flex-col gap-2 p-4">
+      <div className="bg-dark-purple-950 h-16 text-white">bg-dark-purple-950</div>
+      <div className="bg-dark-purple-900 h-16 text-white">bg-dark-purple-900</div>
+      <div className="bg-dark-purple-800 h-16 text-white">bg-dark-purple-800</div>
+      <div className="bg-dark-purple-700 h-16 text-white">bg-dark-purple-700</div>
+      <div className="bg-dark-purple-600 h-16 text-white">bg-dark-purple-600</div>
+      <div className="bg-dark-purple-500 h-16 text-white">bg-dark-purple-500</div>
+      <div className="bg-dark-purple-400 h-16 text-white">bg-dark-purple-400</div>
+      <div className="bg-dark-purple-300 h-16 text-white">bg-dark-purple-300</div>
+      <div className="bg-dark-purple-200 h-16 text-white">bg-dark-purple-200</div>
+      <div className="bg-dark-purple-100 h-16 text-white">bg-dark-purple-100</div>
+      <div className="bg-dark-purple-50 h-16 text-white">bg-dark-purple-50</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
